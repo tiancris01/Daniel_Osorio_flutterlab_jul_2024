6bb20b7ed9229eab00bb5e350a7678df060bfd97
Daniel osorio <tiancris01@gmail.com>
2024-08-01 12:16:17 -0500

    feat: Add lazy loading for images and update app theme

--- lib/src/presentation/screens/home_page.dart
+++ lib/src/presentation/screens/home_page.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:prueba_tecnica_2024/src/domain/entities/documents_entity.dart';
 import 'package:prueba_tecnica_2024/src/presentation/bloc/document_bloc.dart';
 import 'package:prueba_tecnica_2024/src/presentation/bloc/document_satate.dart';
+import 'package:prueba_tecnica_2024/src/presentation/screens/widgets/custom_sliver_app_bar.dart';
+import 'package:prueba_tecnica_2024/src/presentation/screens/widgets/custpm_sliver_list.dart';
 
+/// HomePage es el widget principal de la aplicación, es el encargado de mostrar
+/// la lista de documentos y de manejar el scroll para cargar más documentos
+/// cuando se llega al final de la lista.
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,14 +16,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  /// Controlador del scroll para detectar cuando se llega al final de la lista
   final ScrollController _scrollController = ScrollController();
 
+  /// Inicializa el controlador del scroll y agrega un listener para detectar
   @override
   void initState() {
     super.initState();
     _scrollController.addListener(_scrollListener);
   }
 
+  /// Listener que se ejecuta cuando se llega al final de la lista
   void _scrollListener() {
     final state = DocumentState.of(context);
     if (_scrollController.position.pixels ==
@@ -33,41 +41,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     DocumentBloc state = DocumentState.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Slow App'),
-      ),
       body: StreamBuilder<List<DocumentEntity>>(
         stream: state.documentStream,
         builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          }
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
               child: CircularProgressIndicator(),
             );
           }
           final documents = snapshot.data!;
-          return ListView.builder(
-            controller: _scrollController,
-            itemCount: documents.length + (state.isFetching ? 1 : 0),
-            itemBuilder: (context, index) {
-              final document = documents[index];
-              if (index == documents.length - 1) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              return ListTile(
-                title: Text(document.title),
-                subtitle: Text(document.id),
-                leading: CircleAvatar(
-                  child: Text('${index + 1}'),
+          if (snapshot.hasError) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  snapshot.error.toString(),
+                  style: const TextStyle(color: Colors.red),
                 ),
-              );
-            },
+              ),
+            );
+          }
+          // CustomScrollView para mostrar la lista de documentos
+          return CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              const CustomSliverAppBar(),
+              const SliverToBoxAdapter(
+                child: SizedBox(height: 10),
+              ),
+              CustomSliverList(documents: documents)
+            ],
           );
         },
       ),
